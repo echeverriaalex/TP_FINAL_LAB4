30eafc434d909b8e17253cfe046989eafdd8b273
echeverriaalex <aleex.naahuel@outlook.com>
2021-10-01 11:36:55 -0300

    armamos la estructura de framework

---
+++ Views/nav.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+     <span class="navbar-text">
+          <strong>Framework</strong>
+     </span>
+     <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowAddView">Agregar Alumno</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowListView">Listar Alumnos</a>
+          </li>          
+     </ul>
+</nav>
--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1> TP final lab 4</h1>
-</body>
-</html>
+<?php
+ 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	
+	require "Config/Autoload.php";
+	require "Config/Config.php";
+
+	use Config\Autoload as Autoload;
+	use Config\Router 	as Router;
+	use Config\Request 	as Request;
+		
+	Autoload::start();
+
+	session_start();
+
+	require_once(VIEWS_PATH."header.php");
+
+	Router::Route(new Request());
+
+	require_once(VIEWS_PATH."footer.php");
+?>
