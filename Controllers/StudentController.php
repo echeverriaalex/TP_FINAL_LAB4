bbbe25a340edde173bb64dbda248334ea51cc97e
echeverriaalex <aleex.naahuel@outlook.com>
2021-10-02 18:36:24 -0300

    modifico y agrego nuevas funciones

--- Controllers/StudentController.php
+++ Controllers/StudentController.php
@@ -8,32 +8,42 @@
     {
         private $studentDAO;
 
-        public function __construct()
-        {
-            $this->studentDAO = new StudentDAO();
-        }
+        public function __construct(){$this->studentDAO = new StudentDAO();}
 
-        public function ShowAddView()
-        {
+        public function ShowAddView(){
             require_once(VIEWS_PATH."student-add.php");
         }
 
-        public function ShowListView()
-        {
-            $studentList = $this->studentDAO->GetAll();
+        public function ShowListView(){
 
+            $studentList = $this->studentDAO->GetAll();
             require_once(VIEWS_PATH."student-list.php");
         }
 
-        public function Add($recordId, $firstName, $lastName)
-        {
+        public function ShowMyProfile(){
+
+
+            require_once(VIEWS_PATH."profile.php");
+        }
+
+        public function EditProfile(){
+
+            require_once(VIEWS_PATH."edit-profile.php");
+
+        }
+
+        public function Add($name, $surname, $dni, $phone, $id){
+
+            //$student = new Student($name, $surname, $dni, $phone, $id);
+
             $student = new Student();
-            $student->setRecordId($recordId);
-            $student->setfirstName($firstName);
-            $student->setLastName($lastName);
+            $student->setName($name);
+            $student->setSurname($surname);
+            $student->setDni($dni);
+            $student->setPhone($phone);
+            $student->setId($id);
 
             $this->studentDAO->Add($student);
-
             $this->ShowAddView();
         }
     }
