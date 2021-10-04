0301e84f893a0a464e4181eafc027bb6173a2d73
echeverriaalex <aleex.naahuel@outlook.com>
2021-10-03 21:05:34 -0300

    nav especificamente la el administrador

---
+++ Views/nav-admin.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+     <span class="navbar-text">
+          <a href="<?php echo FRONT_ROOT?>Home/Index"> Home </a>
+     </span>
+     <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/ShowAddView">Add company </a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/ShowEditView">Edit company</a>
+          </li>
+          <li>
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/ShowDeleteView">Delete company</a>
+          </li>
+     </ul>
+</nav>
