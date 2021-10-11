61c2b9a911046b030c4a73969ae89b9fadb00c02
echeverriaalex <aleex.naahuel@outlook.com>
2021-10-11 18:17:31 -0300

    defino constantes que tienen como valor urls de la api

--- Config/Config.php
+++ Config/Config.php
@@ -7,4 +7,10 @@
     define("VIEWS_PATH", "Views/");
     define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
     define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+    define("URL_API", "https://utn-students-api.herokuapp.com/index.html");
+    define("Header_Name","x-api-key");
+    define("Header Value","4f3bceed-50ba-4461-a910-518598664c08");
+    define("Request_URL_Career","https://utn-students-api.herokuapp.com/api/Career");
+    define("Request_URL_JobPosition","https://utn-students-api.herokuapp.com/api/JobPosition");
+    define("Request_URL_Students","https://utn-students-api.herokuapp.com/api/Student");
 ?>
